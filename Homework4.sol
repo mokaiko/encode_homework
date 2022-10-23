f1d5bd5cd0a4553f37163d6da749edc133e54ebc
Mo Kaiko <vipmenghaigang@gmail.com>
2022-10-23 21:15:37 +0800

    Finished homework4

---
+++ Homework4.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract VolcanoCoin {
+
+    uint private totalSupply = 10_000;
+
+    address public owner;
+
+    mapping(address => uint) public balances;
+
+    struct Payment {
+        uint amount;
+        address recipient;
+    }
+
+    mapping(address => Payment[]) private records;
+
+    modifier onlyOwner() {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
+
+    event increaseTotalSupplyEvent(uint indexed);
+    event transferEvent(uint indexed, address indexed);
+
+    constructor() {
+        owner = msg.sender;
+        balances[owner] = totalSupply;
+    }
+
+    function getTotalSupply() public view returns(uint) {
+        return totalSupply;
+    }
+
+    function increase1000() public onlyOwner {
+        totalSupply = totalSupply + 1000;
+        emit increaseTotalSupplyEvent(totalSupply);
+    }
+
+    function transfer(uint _amount, address _recipient) public {
+        require(balances[msg.sender] >= _amount, "Hey! The value you send should be less than your current balance!");
+        balances[msg.sender] -= _amount;
+        balances[_recipient] += _amount;
+        emit transferEvent(_amount, _recipient);
+        addPaymentRecord(_amount, _recipient);
+    }
+
+    function addPaymentRecord(uint _amount, address _recipient) private {
+        records[msg.sender].push(Payment({amount: _amount, recipient: _recipient}));
+    }
+
+    function getPaymentRecords(address _sender) public view returns(Payment[] memory){
+        return records[_sender];
+    }
+
+}
