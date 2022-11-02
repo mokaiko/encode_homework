ad7d4c41be18782c66370af06865b1f5b9911df7
Mo Kaiko <vipmenghaigang@gmail.com>
2022-11-03 02:28:33 +0800

    update hw8

--- test/VolcanoCoinTest.js
+++ test/VolcanoCoinTest.js
@@ -46,9 +46,9 @@ describe("Test VolcanoCoin Contract", function () {
     console.log("contract owner:", await volcanoCoin.owner());
 
     // There are still some bugs here
-    expect(await volcanoCoin.increase1000()).to.not.be.reverted;
-    expect(await volcanoCoin.connect(owner).increase1000()).to.not.be.reverted;
-    expect(await volcanoCoin.connect(otherAccount).increase1000()).to.be.revertedWith("Only owner can do this");
+    expect(await volcanoCoin.increase1000()).to.not.be.reverted;  // Expect: Should NOT be reverted, Expect PASS
+    expect(await volcanoCoin.connect(owner).increase1000()).to.not.be.reverted; // Expect: Should NOT be reverted, Expect PASS
+    expect(await volcanoCoin.connect(otherAccount).increase1000()).to.be.revertedWith("Only owner can do this");  // Expect: Should be reverted, Expect PASS, Actually FAIL
     //await volcanoCoin.connect(otherAccount).increase1000();
 
   });
