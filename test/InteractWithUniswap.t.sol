d388ba1f640407b6bbaa28b7c28914a1e249cda7
Mo Kaiko <vipmenghaigang@gmail.com>
2022-11-24 14:09:43 +0800

    add dai

--- test/InteractWithUniswap.t.sol
+++ test/InteractWithUniswap.t.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract InteractWithUniswapTest is Test {
     uint256 private mainnetFork;
     string private MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
 
-    address payable private binanceAccount =
-        payable(0xDFd5293D8e347dFe59E90eFd55b2956a1343963d);
+    address private constant binanceAccount =
+        0xDFd5293D8e347dFe59E90eFd55b2956a1343963d;
 
-    address private daiStablecoinAddress =
+    address private constant daiStablecoinAddress =
         0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-    address private uniswapV3RouterAddress =
+    address private constant uniswapV3RouterAddress =
         0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    IERC20 DAI = IERC20(daiStablecoinAddress);
 
     function setUp() public {
         mainnetFork = vm.createFork(MAINNET_RPC_URL);
@@ -22,5 +25,6 @@ contract InteractWithUniswapTest is Test {
     function testBalance() public {
         vm.selectFork(mainnetFork);
         console2.log("Binance account ETH:", binanceAccount.balance);
+        console2.log("Binance account DAI:", DAI.balanceOf(binanceAccount));
     }
 }
